1a8d87fa615eaeafac3579587de2bcd9ff47bad8
Canva <canvachen@foxmail.com>
2019-04-03 12:38:39 +0800

    Update util.sh

--- util.sh
+++ util.sh
@@ -2,23 +2,23 @@
 
 function checkParam() {
   # 参数：apk路径
-	if [ $# -ne 1 ]; then
-		echo "Please input an apk file path"
-		exit
-	fi
+  if [ $# -ne 1 ]; then
+    echo "Please input an apk file path"
+    exit
+  fi
 
   # apk后缀：.apk
-	local param=$1
-	local filename=${param##*/}
+  local param=$1
+  local filename=${param##*/}
   if [ "${filename##*.}" != "apk" ]; then
     echo "Please input a correct apk file name"
     exit
   fi
 
   # 文件夹名:apk名称
-	export foldername=${filename%.apk}
-	# apk绝对路径
-	export absolutename=$(cd `dirname $param`; pwd)"/"${filename}
+  export foldername=${filename%.apk}
+  # apk绝对路径
+  export absolutename=$(cd `dirname $param`; pwd)"/"${filename}
 }
 
 function initDir() {
