b3254282a1b34ca4d22a802f72cc49dd7b09162e
Canva <canvachen@foxmail.com>
2019-04-03 12:39:29 +0800

    Update apk-reverse.sh

--- apk-reverse.sh
+++ apk-reverse.sh
@@ -6,28 +6,30 @@ checkParam $1
 initDir
 
 # 执行apktool
+echo "Decompile resource files ..."
+echo `pwd`
 java -jar ../tools/apktool_2.4.0.jar d -f $absolutename
 if [ ! -d "$foldername" ]; then
-	echo "Decompile resource files failed"
-	exit
+  echo "Decompile resource files failed"
+  exit
 fi
 echo "Decompile resource files successfully"
 
 # 执行enjarify
+echo "Decompile dex files ..."
 cd ../tools/enjarify-1.0.3
 echo `pwd`
-suffix=".jar"
-jarname=${foldername}${suffix}
+jarname=${foldername}".jar"
 sh enjarify.sh -f $absolutename -o ${absolutefolder}"/"${jarname}
 cd $absolutefolder
 echo `pwd`
 if [ ! -f "$jarname" ]; then
-	echo "Decompile dex files failed"
-	exit
+  echo "Decompile dex files failed"
+  exit
 fi
 echo "Decompile dex files successfully"
 echo ${absolutefolder}"/"${jarname}
 
 # 执行jd-gui
-echo "View the decompiled files"
+echo "View the decompiled files ..."
 java -jar ../../tools/jd-gui-1.4.1.jar $jarname
